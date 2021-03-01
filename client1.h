ea77f4f7fbadfeaf4a3fb078ba561c2d61d8386f
Thirunayan-manikantan <49241314+Thirunayan-manikantan@users.noreply.github.com>
2021-03-01 11:37:57 +0530

    Add files via upload

---
+++ client1.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include<string>
+#include<WS2tcpip.h>  //header file for winsock functions
+#pragma comment(lib, "ws2_32.lib")  //winsock library file
+#define MAX_BUFFER_SIZE (50000)
+
+class slistener
+{
+public:
+	slistener(std::string ipaddress, int port, std::string msg);
+	~slistener();
+
+	//send the msg to the specified client
+	int send(int clientsocket, std::string msg);
+
+	//initialize win sock
+	bool init();
+
+	//the main loop
+	void run();
+
+	void cleanup();
+
+private:
+
+	//create a socket
+	SOCKET createsocket();
+
+	//connect to server
+	SOCKET connecttoserver();
+
+	std::string m_ipaddress, m_msg;
+	int m_port;
+};
---
+++ main1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+
+#include "server1.h"
+#include "client1.h"
+
+using namespace std;
+
+void listener_messagereceived(clistener* listener, int server, string msg);
+
+void main()
+{
+	string choice;
+	cout << "1) server \t 2) client" << endl;
+	getline(cin, choice);
+
+	if (choice == "server")
+	{
+		string ip = "127.0.0.1";
+		int portnum = 54000;
+		clistener server(ip, portnum, listener_messagereceived);
+		if (server.init())
+		{
+			server.run();
+		}
+	}
+
+	if (choice == "client")
+	{
+		string ip = "127.0.0.1", userinput;
+		int portnum = 54000;
+		cout << "> ";
+		getline(cin, userinput);
+		slistener client(ip, portnum, userinput);
+		if (client.init())
+		{
+			client.run();
+		}
+	}
+}
+void listener_messagereceived(clistener* listener, int server, string msg)
+{
+	listener->send(server, msg);
+}
